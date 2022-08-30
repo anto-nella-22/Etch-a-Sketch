7f9bde1212fde09cf8a71ee7aab46b6c0e8b221a
Antonella <antonella.la01@gmail.com>
2022-08-30 12:55:49 -0600

    Create hover effect Add forEach to loop through all divs Create seperate eventListeners

--- myscript.js
+++ myscript.js
@@ -8,7 +8,23 @@ for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         let gridSquare = document.createElement('div');
         gridSquare.classList.add('square');
+        gridSquare.classList.add('hover');
         document.querySelector('#grid-container').appendChild(gridSquare);
     }
 
 }
+
+//Create hover effect
+let hoverAll = document.querySelectorAll('.hover');
+//Use forEach to addEventListener to all elements
+hoverAll.forEach(function(hoverOne) {
+    //mouse enters div
+    hoverOne.addEventListener('mouseover', (e) => {
+        e.target.setAttribute('style', 'background: pink');
+    }, false);
+    //mouse leaves div
+    hoverOne.addEventListener('mouseout', (e) => {
+        e.target.setAttribute('style', 'background: white');
+    }, false);
+})
+
